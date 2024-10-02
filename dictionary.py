2606b96328801e72652a3b8d25aaef0001a9b73f
Ramirez <lramirez99@cps.k12.il.us>
2024-10-02 12:06:28 -0500

    first commit

---
+++ dictionary.py
@@ -0,0 +1,58 @@
+# dictionary = a collection of {key: values} pairs
+
+capitals = {"Usa": "Washington D.C.","India": "New delhi","China": "Beijing", "Russia": "Moscow"}
+
+# print(dir(capitals))
+#print (help(capital))
+
+# print (capitals.get("USA"))
+
+# print(capital.get(japan))
+
+# if capitals.get("japan")
+#   print("that capital exist")
+# else
+#     print("That print doesnt exist")
+
+
+# how to update list
+capitals.update({"Germant": "berlin"})
+
+print(capitals)
+
+capitals.update({"Mexico": "Mexico city","Italy" : "Rome","France":"paris"})
+
+print(capitals)
+
+# # remove from list
+#capitals.pop("China")
+
+# # removes last thing in dictionary
+#capitals.popitem
+
+## clear dicionary
+#capitals.clear()
+
+# # to get only keys 
+# keys = capitals.keys()
+
+# print(keys)
+
+## to get only values 
+# values = capitals value
+
+#print(value)
+
+## for loop
+
+# for keys in capitals.keys()
+#   print(key)
+
+# for value in capitals.values()
+#   print(value)
+
+#items method
+
+# items = capitals.item()
+#for keys value in capitals.item()
+#  print(f"{keys}: {values}")
